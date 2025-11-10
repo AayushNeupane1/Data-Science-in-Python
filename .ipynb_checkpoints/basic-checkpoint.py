96c916f472318bcee850d5cfc700e699b8c94c61
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-11-10 11:59:29 +0545

    first commit

---
+++ .ipynb_checkpoints/basic-checkpoint.py
@@ -0,0 +1,32 @@
+# b="welcome to python programming"
+# print(f"Hello,{b}")
+
+# a=int(input("enter the number"))
+# if a%2==0:
+#     print(f"The number {a} is even")
+
+# else: 
+#     print(f"The number {a} is odd")
+
+#class task 2
+
+# name="Aayush Neupane" #data type is string 
+# age=25 #data type is integer
+# height=5.6 #data type is float
+# is_student=True #data type is boolean
+# print(name)
+
+# age=int(5)
+# age=age+10
+# print(f"My name is {name} and my age is {age+6}")
+# print(is_student)
+
+# task 3
+
+x=20
+y=10
+print(x*y)
+
+
+
+
